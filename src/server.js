ba042f91e18b94b2a0e27577fc21d4d11458c5d2
MauricioRosasKsquare <Mauricio.Rosas@theksquaregroup.com>
2021-11-03 04:17:35 -0600

    New error message

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(routes);
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Welcome to my API',
+    message: 'Welcome to my API try again',
   });
 });
 
